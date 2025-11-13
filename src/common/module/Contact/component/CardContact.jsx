0d1a4f46b2fd8ba5462617321bbce9ba70803555
Jody Gs <jodygonzales.pro@gmail.com>
2025-11-13 20:13:06 +0100

    mentions legales

--- src/common/module/Contact/component/CardContact.jsx
+++ src/common/module/Contact/component/CardContact.jsx
@@ -1,4 +1,3 @@
-import Image from "@/common/component/element/Image";
 import React from "react";
 import ContactForm from "./ContactForm";
 import ComponentTransition from "@/common/component/element/ComponentTransition";
@@ -21,27 +20,6 @@ const CardContact = () => {
           <ContactForm />
         </div>
 
-        <ComponentTransition delay={0.2} className="w-full h-auto overflow-hidden rounded-3xl  md:basis-[47%] ">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253840.65638805012!2d106.66470295433655!3d-6.229379591448096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3e945e34b9d%3A0x5371bf0fdad786a2!2sJakarta%2C%20Daerah%20Khusus%20Ibukota%20Jakarta!5e0!3m2!1sid!2sid!4v1701852183072!5m2!1sid!2sid"
-            width="100%"
-            height="100%"
-            className="border-none h-[350px] px-0 rounded-3xl"
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </ComponentTransition>
-        <ComponentTransition delay={0.3} className="w-full h-[350px] overflow-hidden rounded-3xl  md:basis-[47%]">
-          <Image 
-          src="https://images.unsplash.com/photo-1653745289778-bacf6d8a96e5?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="image"
-          width={400}
-          height={400}
-          priority
-          className="w-full h-full object-cover scale-110"
-          />
-        </ComponentTransition>
       </div>
     </>
   );
